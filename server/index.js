c19993b10c0e39e03a48a466763afefb741800d2
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-10 10:55:34 -0600

    Import axios

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 
