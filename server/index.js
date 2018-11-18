851a2e74b09b202a71c8d9e7f23b4d4ad30bf3c2
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-18 14:45:54 -0600

    Import bcrypt and express-sessions to the server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const bcrypt = require('bcrypt-nodejs');
 const io = require('socket.io');
 
 const app = express();
