ceecc950e76709b9d7e0ace0124105c62206beaa
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-21 17:40:41 -0600

    Add framework for user to update profile

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => (
   <div id="Home" >
     {/* loop autoPlay */}
-    <video loop autoPlay muted className="theVid" >
+    <video loop autoPlay className="theVid" >
       <source src="LasVegasTimelapse.mp4" type="video/mp4" />
     </video>
   </div>
--- client/src/components/NestedComponents/UserPortal.jsx
+++ client/src/components/NestedComponents/UserPortal.jsx
@@ -1,23 +1,71 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Button, Alert, Input, Row, Col } from 'reactstrap';
 import axios from 'axios';
+import Modal from './Modal.jsx';
+
 
 class UserPortal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       // adminDisplay: 0
+      modalOpen: false,
+      showProfile: false
     };
+    this.showUserProfile = this.showUserProfile.bind(this);
   }
-  // componentDidMount() {
+  componentDidMount() {
   //   axios.get('/api/check', props.userInfo)
   //    .then(status => this.setState({ adminDisplay: status }));
-  // }
+  }
+  
+  showUserProfile() {
+    this.setState({ showProfile: !this.state.showProfile });
+  }
 
   render() {
     return (
       <div>
         <h1>Welcome Back, {this.props.userInfo.name}!</h1>
+        { 
+          this.props.userInfo.email === null || this.props.userInfo.phone === null || this.props.userInfo.name === null ?
+            <section>
+              <Alert color="primary" >
+                Some data is missing from your profile, please update as soon as possible to ensure you can be contacted with updates on the trip.
+              </Alert>
+            </section> 
+          : null
+        }
+        {
+          this.state.showProfile ? 
+            <section>
+              <Row className="profile" >
+                <Col><em>Name: <Input placeholder={this.props.userInfo.name} /></em></Col>
+                <Col><em>Email: <Input placeholder={this.props.userInfo.email}/></em></Col>
+                <Col><em>Phone: <Input placeholder={this.props.userInfo.phone}/></em></Col>
+                <Col><Button style={{marginTop: '23px'}}  outline color="primary" block >Update Info</Button></Col>
+              </Row>
+            </section>
+            : null
+            // <section>
+            //   <Row className="profile" >
+            //     <Col><em>Name: {this.props.userInfo.name}</em></Col>
+            //     <Col><em>Email: {this.props.userInfo.email}</em></Col>
+            //     <Col><em>Phone: {this.props.userInfo.phone}</em></Col>
+            //   </Row>
+            // </section>
+        }
+        {
+          this.state.showProfile ? 
+          <Button outline color="secondary" block onClick={this.showUserProfile} >Cancel</Button> :
+          <Button outline color="secondary" block onClick={this.showUserProfile} >Update Info</Button>
+        }
+        <section>
+
+          <div>
+
+          </div>
+        </section>
       </div>
     )
   }
