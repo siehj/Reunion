715f5e9005ba4b503a793ecf22578791baeedd46
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-21 17:46:42 -0600

    Try to get video to play for deployed. Stopped

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Home = () => (
   <div id="Home" >
-    {/* loop autoPlay */}
     <video loop autoPlay className="theVid" >
       <source src="LasVegasTimelapse.mp4" type="video/mp4" />
     </video>
