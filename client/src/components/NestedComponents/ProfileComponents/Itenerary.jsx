bf4738a22f03077b24f6986cca97b6d21924c0fc
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-27 00:51:37 -0600

    Refactor rendering for data to better fit user story/use case

--- client/src/components/NestedComponents/ProfileComponents/Itenerary.jsx
+++ client/src/components/NestedComponents/ProfileComponents/Itenerary.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Itenerary = (props) => {
   return (
-    <div>
+    <div className="userPortalMiniScreen" >
       {
         Object.keys(props.sched).map((day, idx) => {
           return (
--- client/src/components/NestedComponents/ProfileComponents/Voting.jsx
+++ client/src/components/NestedComponents/ProfileComponents/Voting.jsx
@@ -1,21 +1,46 @@
 import React from 'react';
-import { Input, Button } from 'reactstrap'
+import { Input, Button, Row, Col } from 'reactstrap'
 
 const VotingComponent = (props) => {
   console.log(props.item)
   return (
-    <div>
+    <div className="userPortalMiniScreen" >
       {/* <Input></Input> <Button>Add Location</Button> */}
       {
         props.item.map((topic, idx) => {
           return (
             <div key={idx} className="votingTopic">
-              <h3>{topic.Title}</h3>
-              <em>{topic.Summary}</em>
-              {/* {
+              <h3 className="text-center" >{topic.Title}</h3>
+              <h6 className="text-center" >{topic.Summary}</h6>
+              {
                 Array.isArray(topic.options) ? 
-
-              } */}
+                topic.options.map((op, i) => {
+                  return (
+                    <Row className="votingOptions" key={i}>
+                      <Col sm="8" md="8" lg="8">
+                        <h5>Name: {op.name}</h5>
+                        <em>Location: {op.location}</em>
+                      </Col>
+                      <Col sm="2" md="2" lg="2"><h5>{op.votes}</h5></Col>
+                      <Col sm="2" md="2" lg="2"><Button>Vote</Button></Col>
+                    </Row>
+                  )
+                })
+                : 
+                <Row className="votingOptions" className="text-center" >
+                {
+                  Object.keys(topic.options).map((op, j) => {
+                  return (
+                      <Col sm="6" md="6" lg="6" key={j}  >
+                        <h5>{op}</h5>
+                        <h5>{topic.options[op]}</h5>
+                        <Button>Vote</Button>
+                      </Col>
+                  )
+                  })
+                }
+                </Row>
+              }
             </div>
           )
       })
