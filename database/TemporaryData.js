23f60b707d8bc8989b39f0f274067a10c0c9c328
Sieh Johnson <johnson_sieh@yahoo.com>
2018-11-26 23:42:54 -0600

    Add temporary fake data as place holders

---
+++ database/TemporaryData.js
@@ -0,0 +1,39 @@
+
+const fake = [
+  {
+    Title: 'Bottle Service', 
+    Summary: "Vote for the club you'd like to get bottle service.",
+    options: [
+      {
+        name: "",
+        location: "",
+        votes: 0
+      }
+    ] 
+  },
+  {
+    Title: 'Dinner', 
+    Summary: "Vote where you'd like to go for dinner on Friday night.",
+    options: [
+      {
+        
+      }
+    ] 
+  },
+  {
+    Title: 'Brunch', 
+    Summary: "Vote where you'd like to get brunch.",
+    options: [
+      {
+
+      }
+    ] 
+  },
+  {
+    Title: 'Friday night or Saturday night?', 
+    Summary: "Vote if you'd like to go to dinner and club on Friday or Saturday night.",
+    options: { yes : 0, no : 0 } 
+  }
+]
+
+export default fake;
