ec557b51725a676d8f254b6096fee275b5def228
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-27 14:13:40 -0600

    Create a userProfile file to refactor user related server logic

---
+++ server/Routes/userProfile.js
@@ -0,0 +1,21 @@
+const db = require('../../database');
+
+module.exports = {
+  'updateUser' : (req, res) => {
+    let updatedInfo = req.body;
+    let id = updatedInfo.id;
+    delete updatedInfo.id;
+  
+    db.updateUser(id, updatedInfo)
+      .then(() => {
+        db.getWithId(id)
+          .then(({ rows }) => {
+            return rows[0];
+          })
+          .then(userInfo => res.send(userInfo))
+          .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));
+  },
+
+};
