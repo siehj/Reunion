358af1816a536a85131deb45041be4a0ea802863
Sieh Johnson <johnson_sieh@yahoo.com>
2019-01-27 21:55:24 -0600

    Update the join the fun component page

--- client/src/components/JoinTheFun.jsx
+++ client/src/components/JoinTheFun.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const JoinTheFun = () => (
-  <div id="JTF" >Join The Fun</div>
+  <div id="JTF" >
+    <h1>Join The Fun</h1>
+    <div>
+      <h1 className="text-center">...COMING SOON...</h1>
+      <p className="text-center">
+        Check out the "About" page to get more information about the app and future features.
+      </p>
+    </div>
+  </div>
 );
 
 export default JoinTheFun;
